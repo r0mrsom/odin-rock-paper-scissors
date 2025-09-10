48ab2c3d68c1ecd3ffe9918e11446a6998a20de8
r0mrsom <173564029+r0mrsom@users.noreply.github.com>
2025-09-10 20:38:20 +0800

    Add playRound() function to script.js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let computerScore = 0;
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random()*3) + 1;
-    console.log(randomNumber);
     if (randomNumber == 1) {
         return "rock";
     }
@@ -12,13 +11,58 @@ function getComputerChoice() {
         return "paper";
     }
     else {
-        return "scissor";
+        return "scissors";
     }
 };
 
 function getHumanChoice() {
-    let humanChoice = prompt("Choose from the following: rock, paper, scissor");
+    let humanChoice = prompt("Choose from the following: rock, paper, scissors");
     return humanChoice;
 };
 
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    let formatHumanChoice = humanChoice.toLowerCase();
+    let computerIncrement = 0;
+    let humanIncrement = 0;
+    if (humanChoice == 'rock') {
+        if (computerChoice == 'paper') {
+            computerIncrement = 1;
+        }
+        else if (computerChoice == 'scissors') {
+            humanIncrement = 1;
+        }
+    }
+    else if (humanChoice == 'paper') {
+        if (computerChoice == 'scissors') {
+            computerIncrement = 1;
+        }
+        else if (computerChoice == 'rock') {
+            humanIncrement = 1;
+        }
+    }
+    else if (humanChoice == 'scissors') {
+        if (computerChoice == 'rock') {
+            computerIncrement = 1;
+        }
+        else if (computerChoice == 'paper') {
+            humanIncrement = 1;
+        }
+    }
+    if (humanIncrement == 1) {
+        console.log(`You Win! ${humanChoice[0].toUpperCase() + humanChoice.slice(1)} beats ${computerChoice[0].toUpperCase() + computerChoice.slice(1)}.`)
+        humanScore += 1;
+    }
+    else if (computerIncrement == 1) {
+        console.log(`You Lose! ${computerChoice[0].toUpperCase() + computerChoice.slice(1)} beats ${humanChoice[0].toUpperCase() + humanChoice.slice(1)}.`)
+        computerScore += 1;
+    }
+    else (
+        console.log(`Draw! No one wins.`)
+    )
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
